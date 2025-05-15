10c5c654737ba536c51407b97d4e8ff62f45cb94
kusbah <abdkusbah2@gmail.com>
2025-05-16 01:05:11 +0300

    make the MainApp and make class Passenger

--- src/travel/MainApp.java
+++ src/travel/MainApp.java
@@ -4,7 +4,10 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		
-
+		
+		Passenger p1 = new Passenger(0, "Abdullah", "4848484");
+		Passenger p2 = new Passenger(1, "mohmad", "7878787");
+		p1.print_info();
 	}
 
 }
---
+++ src/travel/Passenger.java
@@ -0,0 +1,40 @@
+package travel;
+
+public class Passenger {
+	private int id;
+	private String name,phone;
+
+	public Passenger(int id ,String name,String phone) {
+		
+		this.id = id;
+		this.name = name;
+		this.phone = phone;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public void print_info() {
+		System.out.println("Name: "+name+"\nPhone: "+phone);
+	}
+}
