9c25f85553646cad4faeed5fb21ac6a80a03c2c7
kusbah <abdkusbah2@gmail.com>
2025-05-18 23:04:34 +0300

    complete vehicel

---
+++ src/travel/Vehicle.java
@@ -0,0 +1,12 @@
+
+abstract public class Vehicle {
+
+	private String comapny;
+	private String modle;
+	private int id;
+	void AddVehicle(int id,String comapny,String modele) {
+		this.id = id;
+		this.comapny = comapny;
+		this.modle = modle;
+	}
+}
