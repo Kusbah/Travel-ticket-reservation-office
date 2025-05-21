db42be224f69b36a6ea01b09289ff81f458a8ba3
kusbah <abdkusbah2@gmail.com>
2025-05-21 21:04:47 +0300

    test the main app

--- src/travel/MainApp.java
+++ src/travel/MainApp.java
@@ -1,17 +1,23 @@
 package travel;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class MainApp {
 
 	public static void main(String[] args) {
 		
+		Plane plane = new Plane();
+		plane.AddVehicle(0, "AirBus", "ZT100");
+		Trip jordenPalstineTrip = new Trip();
+		LocalTime tripDTime = LocalTime.now();
+		LocalDate tripDate = LocalDate.now();
 		
-		Passenger p1 = new Passenger(0, "Abdullah", "4848484");
-		Passenger p2 = new Passenger(1, "mohmad", "7878787");
-		p1.print_info();
+		Passenger basemPassenger = new Passenger(2151561, "Basem", "78984516");
 		
-		Bus bus1 = new Bus();
-		bus1.AddVehicle(0, "BMW", "M4");
-		bus1.print_info();
+		Trip trip =new Trip();
+		trip.createTrip(0, "Jorder", "Palstine", tripDate, tripDTime, plane);
+		
 	}
 
 }
